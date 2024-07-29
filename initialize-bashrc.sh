1aec5a84aeeea331167a78e916424bf9b5e092c3
Eliana <135545036+odessaeliana@users.noreply.github.com>
2024-07-29 03:48:27 +0200

    Update initialize-bashrc.sh

--- initialize-bashrc.sh
+++ initialize-bashrc.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 # Check if a parameter is passed; if not, echo usage and exit
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 [Repositories Directory]"
+  echo "Usage: $0 [Reposotories Directory]"
   echo "Usage: $0 ~/."
   exit 1
 fi
@@ -10,12 +9,9 @@ fi
 DirectoryRepos=$1
 
 # Write .bashrc linking commands with the provided directory
-cat << EOF > ~/.bashrc
-export DirectoryRepos=$DirectoryRepos
+echo "export DirectoryRepos=$DirectoryRepos
 source \$DirectoryRepos/bash-profile/.bash_profile
-source \$DirectoryRepos/bash-profile/.bash_profile_personal
-EOF
+source \$DirectoryRepos/bash-profile/.bash_profile_personal" > ~/.bashrc
 
 # Source the updated .bashrc to apply changes
 source ~/.bashrc
->>>>>>> 6379f74b7d2ba11d6b598f364436001a5b518250
