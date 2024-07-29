3f1a6ca13587579ce0fb1141973c577bb3c7a289
Eliana <135545036+odessaeliana@users.noreply.github.com>
2024-07-29 04:07:09 +0200

    Update initialize-bashrc.sh

--- initialize-bashrc.sh
+++ initialize-bashrc.sh
@@ -12,9 +12,9 @@ DirectoryRepos=$1
 
 # Write .bashrc linking commands with the provided directory
 cat <<EOF > ~/.bashrc
-export DirectoryRepos=$DirectoryRepos
-source \$DirectoryRepos/bash-profile/.bash_profile
-source \$DirectoryRepos/bash-profile/.bash_profile_personal
+export DirectoryRepos=/home/bongiswaeliana/bash-profile
+source $DirectoryRepos/.bash_profile
+source $DirectoryRepos/.bash_profile_personal
 EOF
 
 # Source the updated .bashrc to apply changes
